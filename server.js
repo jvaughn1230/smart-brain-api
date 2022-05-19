2eeb009899117baa857ae429b42bf891417b1998
Jeffrey Vaughn <jvaughn1230@gmail.com>
2022-05-19 13:38:37 -0700

    correcting typo

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const knex = require('knex');
 
 const register = require('./controllers/register');
-const signin = require('./controllers/signin');
+const signin = require('./controllers/signIn');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
@@ -25,7 +25,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/', (req, res)=>{res.send('it is working');});
-app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)});
+app.post('/signIn', (req, res) => {signin.handleSignin(req, res, db, bcrypt)});
 app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)});
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)});
 app.put('/image', (req, res) => {image.handleImage(req, res, db)});
